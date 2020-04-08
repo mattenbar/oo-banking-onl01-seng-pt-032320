442a85edf4ae6f3f427d9c74903bb9c7e17f0b0f
matthew enbar <matthew.enbar@gmail.com>
2020-04-08 16:33:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && last_transaction != amount
+    if valid? 
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
